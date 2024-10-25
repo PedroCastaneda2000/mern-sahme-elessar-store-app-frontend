dced2f1ef2a4430bdb83a6943ee20127e5c266d5
Pedro Castaneda <dgcastaneda2000@gmail.com>
2024-10-24 20:32:28 -0400

    first commit redo

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -1,43 +1,12 @@
 // This file will have the User's Menu after login into the site. inside the MainNav.tsx
 import { useAuth0 } from "@auth0/auth0-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
 import { Link } from "react-router-dom";
-import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 
 const UsernameMenu = () => {
   const { logout } = useAuth0();
 
   return (
-    // <DropdownMenu>
-    //   <DropdownMenuTrigger className="flex items-center text-16sm font-light font-inter hover:text-orange-500">
-    //     User Profile
-    //   </DropdownMenuTrigger>
-    //   <DropdownMenuContent className="font-inter">
-    //     <DropdownMenuItem>
-    //       <Link
-    //         to="/user-profile"
-    //         className="font-medium text-16sm hover:text-orange-500"
-    //       >
-    //         User Profile
-    //       </Link>
-    //     </DropdownMenuItem>
-    //     <Separator />
-    //     <DropdownMenuItem>
-    //       <Button
-    //         onClick={() => logout()}
-    //         className="flex-2 font-bold bg-orange-500"
-    //       >
-    //         Log Out
-    //       </Button>
-    //     </DropdownMenuItem>
-    //   </DropdownMenuContent>
-    // </DropdownMenu>
     <div className="flex flex-col gap-4">
       <Link
         to="/user-profile"
