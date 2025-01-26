54c868ff9b16ae6b27b2f69a45e5cdb1bb00248e
Pedro Castaneda <dgcastaneda2000@gmail.com>
2025-01-25 22:42:30 -0500

    final again

--- src/pages/OrderStatusPage.tsx
+++ src/pages/OrderStatusPage.tsx
@@ -1,16 +1,31 @@
 import { useGetMyOrders } from "@/api/OrderApi";
 import OrderStatusDetail from "@/components/OrderStatusDetail";
 import OrderStatusHeader from "@/components/OrderStatusHeader";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const OrderStatusPage = () => {
   const { orders, isLoading } = useGetMyOrders();
+  const navigate = useNavigate();
 
   if (isLoading) {
     return "Loading...";
   }
 
   if (!orders || orders.length === 0) {
-    return "No orders found";
+    return (
+      <div className="flex flex-col items-center justify-center gap-6">
+        <h1 className="text-28md xl:text-40lg text-color-dark text-center font-serif font-normal">
+          Sorry, no active orders...
+        </h1>
+        <Button
+          onClick={() => navigate("/products")}
+          className="text-14sm xl:text-16sm bg-button-primary hover:bg-button-primary-hover text-color-light h-11 w-full max-w-[224px] rounded-sm font-serif font-medium"
+        >
+          View Gallery
+        </Button>
+      </div>
+    );
   }
 
   return (
